1588b040f02ce07f0d4c4521fefa8741e346d7c7
chenda <490178988@qq.com>
2020-07-14 18:02:37 +0800

    1、修改开发匹配模式

--- src/main.js
+++ src/main.js
@@ -6,6 +6,11 @@ import store from './store'
 
 Vue.config.productionTip = false
 
+console.log('mode', process.env.VUE_APP_BASE_URL)
+
+/* https://www.jianshu.com/p/d4ffb5d2e2be 参考配置链接 */
+// https://www.cnblogs.com/wangjae/p/12610490.html
+
 new Vue({
   router,
   store,
